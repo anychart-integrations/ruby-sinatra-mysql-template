2d59c3780d87b8bc2a4ac965e8f1c796746a277a
ilevd <ilevd@mail.ru>
2016-11-25 10:32:11 +0700

    Reformat code

--- main.rb
+++ main.rb
@@ -2,27 +2,17 @@ require 'sinatra'
 require 'mysql2'
 require 'json'
 
-Mysqlclient = Mysql2::Client.new(:host => "localhost",
-	:username => "anychart_user",
-	:password => "anychart_pass",
-	:database => "anychart_db")
+Mysqlclient = Mysql2::Client.new(host: 'localhost',
+                                 username: 'anychart_user',
+                                 password: 'anychart_pass',
+                                 database: 'anychart_db')
 
 def get_top_fruits
-	Mysqlclient.query("SELECT name, value FROM fruits ORDER BY value DESC LIMIT 5", :as => :hash).to_a
+  Mysqlclient.query('SELECT name, value FROM fruits ORDER BY value DESC LIMIT 5', :as => :hash).to_a
 end
 
-get "/" do 
-	data = get_top_fruits
-	chart = {
-		:chart => {
-			:type => "pie",
-			:title => "Top 5 fruits",
-			:data => data,
-			:container => "container"
-		}
-	}
-	erb :main, :locals => {
-		:chartData => chart.to_json,
-		:title => "Anychart Ruby template"
-	}
+get '/' do
+  data = get_top_fruits
+  chart = {chart: {type: 'pie', title: 'Top 5 fruits', data: data, container: 'container'}}
+  erb :main, locals: {title: 'Anychart Ruby template', chartData: chart.to_json}
 end
